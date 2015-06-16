012c60eac377a9a517fadfe7ca6359c22058a2af
unknown <littenli@LITTENLI-PC7.tencent.com>
2015-06-16 14:48:30 +0800

    修复移动侧标签不显示问题

--- source/js/pc.js
+++ source/js/pc.js
@@ -21,7 +21,6 @@ define([], function(){
 		var tags = $(".tagcloud a");
 		tags.css({"font-size": "12px"});
 		for(var i=0,len=tags.length; i<len; i++){
-			//var num = parseInt(Math.random()*5+1);
 			var num = tags.eq(i).html().length % 5 +1;
 			tags[i].className = "";
 			tags.eq(i).addClass("color"+num);
