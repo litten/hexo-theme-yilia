f2345b4c8c810ca79e8675649870926581690462
Robin Zhang <84084888@qq.com>
2015-09-23 02:11:55 +0800

    Revert "Update pc.js"
    This reverts commit 0ff04d372a378ab31612d0d1c8005918a9ee9283.

--- source/js/pc.js
+++ source/js/pc.js
@@ -1,122 +1,100 @@
-require([], function() {
+define([], function(){
 
-  var isMobileInit = false;
-  var loadMobile = function() {
-    require(['/js/mobile.js'], function(mobile) {
-      mobile.init();
-      isMobileInit = true;
-    });
-  }
-  var isPCInit = false;
-  var loadPC = function() {
-    require(['/js/pc.js'], function(pc) {
-      pc.init();
-      isPCInit = true;
-    });
-  }
+	var Tips = (function(){
 
-  var browser = {
-    versions: function() {
-      var u = window.navigator.userAgent;
-      return {
-        trident: u.indexOf('Trident') > -1, //IE内核
-        presto: u.indexOf('Presto') > -1, //opera内核
-        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-        iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
-        iPad: u.indexOf('iPad') > -1, //是否为iPad
-        webApp: u.indexOf('Safari') == -1, //是否为web应用程序，没有头部与底部
-        weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
-      };
-    }()
-  }
+		var $tipBox = $(".tips-box");
 
-  var resize_video = function() { //iframe高度自适应
-  var first_container = $(".video-container").first()
-  if (!first_container) return;
-  var w = first_container.parent().width();
-  var h = w * 9 / 16;
-  $(".video-container iframe").css({"height": h, "width": w});
-  $(".video-container iframe").css({"height": 500*0.97647059});
-  }
+		return {
+			show: function(){
+				$tipBox.removeClass("hide");
+			},
+			hide: function(){
+				$tipBox.addClass("hide");
+			},
+			init: function(){
+				
+			}
+		}
+	})();
 
-  $(window).bind("load", resize_video);
+	var resetTags = function(){
+		var tags = $(".tagcloud a");
+		tags.css({"font-size": "12px"});
+		for(var i=0,len=tags.length; i<len; i++){
+			var num = tags.eq(i).html().length % 5 +1;
+			tags[i].className = "";
+			tags.eq(i).addClass("color"+num);
+		}
+	}
 
-  $(window).bind("resize", function() {
-    //if(isMobileInit && isPCInit){
-    //	$(window).unbind("resize");
-    //	return;
-    //}
-    var w = $(window).width();
-    if (w >= 700) {
-      loadPC();
-    } else {
-      loadMobile();
-    }
-    resize_video();
-  });
+	var slide = function(idx){
+		var $wrap = $(".switch-wrap");
+		$wrap.css({
+			"transform": "translate(-"+idx*100+"%, 0 )"
+		});
+		$(".icon-wrap").addClass("hide");
+		$(".icon-wrap").eq(idx).removeClass("hide");
+	}
 
-  if (browser.versions.mobile === true || $(window).width() < 700) {
-    loadMobile();
-  } else {
-    loadPC();
-  }
+	var bind = function(){
+		var switchBtn = $("#myonoffswitch");
+		var tagcloud = $(".second-part");
+		var navDiv = $(".first-part");
+		switchBtn.click(function(){
+			if(switchBtn.hasClass("clicked")){
+				switchBtn.removeClass("clicked");
+				tagcloud.removeClass("turn-left");
+				navDiv.removeClass("turn-left");
+			}else{
+				switchBtn.addClass("clicked");
+				tagcloud.addClass("turn-left");
+				navDiv.addClass("turn-left");
+				resetTags();
+			}
+		});
 
-  //是否使用fancybox
-  if (yiliaConfig.fancybox === true) {
-    require(['/fancybox/jquery.fancybox.js'], function(pc) {
-      var isFancy = $(".isFancy");
-      if (isFancy.length != 0) {
-        var imgArr = $(".article-inner img");
-        for (var i = 0, len = imgArr.length; i < len; i++) {
-          var src = imgArr.eq(i).attr("src");
-          var title = imgArr.eq(i).attr("alt");
-          imgArr.eq(i).replaceWith("<a href='" + src + "' title='" + title + "' rel='fancy-group' class='fancy-ctn fancybox'><img src='" + src + "' title='" + title + "'></a>");
-        }
-        $(".article-inner .fancy-ctn").fancybox();
-      }
-    });
+		var timeout;
+		var isEnterBtn = false;
+		var isEnterTips = false;
 
-  }
-  //是否开启动画
-  if (yiliaConfig.animate === true) {
+		$(".icon").bind("mouseenter", function(){
+			isEnterBtn = true;
+			Tips.show();
+		}).bind("mouseleave", function(){
+			isEnterBtn = false;
+			setTimeout(function(){
+				if(!isEnterTips){
+					Tips.hide();
+				}
+			}, 100);
+		});
 
-    require(['/js/jquery.lazyload.js'], function() {
-      //avatar
-      $(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
-      $(".js-avatar")[0].onload = function() {
-        $(".js-avatar").addClass("show");
-      }
-    });
+		$(".tips-box").bind("mouseenter", function(){
+			isEnterTips = true;
+			Tips.show();
+		}).bind("mouseleave", function(){
+			isEnterTips = false;
+			setTimeout(function(){
+				if(!isEnterBtn){
+					Tips.hide();
+				}
+			}, 100);
+		});
 
-    if (yiliaConfig.isHome === true) {
-      //content
-      function showArticle() {
-        $(".article").each(function() {
-          if ($(this).offset().top <= $(window).scrollTop() + $(window).height() && !($(this).hasClass('show'))) {
-            $(this).removeClass("hidden").addClass("show");
-            $(this).addClass("is-hiddened");
-          } else {
-            if (!$(this).hasClass("is-hiddened")) {
-              $(this).addClass("hidden");
-            }
-          }
-        });
-      }
-      $(window).on('scroll', function() {
-        showArticle();
-      });
-      showArticle();
-    }
+		$(".tips-inner li").bind("click", function(){
+			var idx = $(this).index();
+			slide(idx);
+			Tips.hide();
+		});
+	}
 
-  }
+	
 
-  //是否新窗口打开链接
-  if (yiliaConfig.open_in_new == true) {
-    $(".article a[href]").attr("target", "_blank")
-  }
-
+	return {
+		init: function(){
+			resetTags();
+			bind();
+			Tips.init();
+		}
+	}
 });
