dd1c3a837d300d3d748f8ea4f737ab7a29070f62
MOxFIVE <mo10005@outlook.com>
2016-02-27 21:15:27 +0800

    fix: labels's background color 标签背景色
    https://github.com/MOxFIVE/M-Hexo-Blog/issues/27

--- source/js/pc.js
+++ source/js/pc.js
@@ -17,16 +17,6 @@ define([], function(){
         }
     })();
 
-    var resetTags = function(){
-        var tags = $(".tagcloud a");
-        tags.css({"font-size": "12px"});
-        for(var i=0,len=tags.length; i<len; i++){
-            var num = tags.eq(i).html().length % 5 +1;
-            tags[i].className = "";
-            tags.eq(i).addClass("color"+num);
-        }
-    }
-
     var slide = function(idx){
         // 修复IE10+切换无效的bug
         var $wrap = $(".switch-wrap"),
