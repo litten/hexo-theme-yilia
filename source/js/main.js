048905522c336491a7a48879d91984e0538fc582
MOxFIVE <mo10005@outlook.com>
2016-04-02 01:38:56 +0800

    fix: case sensitivity of GitHub.png
    Display GitHub icon correctly when the filename is “github.png”
    
    #61

--- source/js/main.js
+++ source/js/main.js
@@ -166,4 +166,15 @@ require([], function (){
         });
     }
 
+    if ($("#GitHub").length) {
+        $.ajax({
+            url: yiliaConfig.rootUrl + 'img/GitHub.png',
+            type: 'HEAD',
+            error: function() {
+                var setUrl = "url(" + yiliaConfig.rootUrl + "img/github.png)"
+                $("#GitHub").css("background-image", setUrl);
+            }
+        });
+    }
+
 });
