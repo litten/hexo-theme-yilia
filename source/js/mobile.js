3c02de2deef03b2367f4b3082223b65d1911dcfa
MOxFIVE <mo10005@outlook.com>
2016-04-26 14:11:53 +0800

    style: translate mobile slider
    #80

--- source/js/mobile.js
+++ source/js/mobile.js
@@ -34,9 +34,12 @@ define([], function(){
         $tag = document.getElementById("js-tagcloud");
         $aboutme = document.getElementById("js-aboutme");
         $friends = document.getElementById("js-friends");
-        var tagStr = $tag?'<span class="viewer-title">标签</span><div class="viewer-div tagcloud" id="js-mobile-tagcloud"></div>':"";
-        var friendsStr = $friends?'<span class="viewer-title">友情链接</span><div class="viewer-div friends" id="js-mobile-friends"></div>':"";
-        var aboutmeStr = $aboutme?'<span class="viewer-title">关于我</span><div class="viewer-div aboutme" id="js-mobile-aboutme"></div>':"";
+        function menuList(name) {
+            return $("link.menu-list").attr(name);
+        };
+        var tagStr = $tag?'<span class="viewer-title">'+ menuList("tags") + '</span><div class="viewer-div tagcloud" id="js-mobile-tagcloud"></div>':"";
+        var friendsStr = $friends?'<span class="viewer-title">'+ menuList("friends") + '</span><div class="viewer-div friends" id="js-mobile-friends"></div>':"";
+        var aboutmeStr = $aboutme?'<span class="viewer-title">'+ menuList("about") + '</span><div class="viewer-div aboutme" id="js-mobile-aboutme"></div>':"";
 
         $viewer.innerHTML = '<div id="viewer-box">\
         <div class="viewer-box-l">\
