6c43058cc303593aa0a5028c05937aec1ca12ed3
liujunjiang <liujunjiang@szxhinfo.com>
2018-04-11 17:41:20 +0800

    添加微信支持

--- source-src/js/main.js
+++ source-src/js/main.js
@@ -1,7 +1,7 @@
 // 样式
 import '../css/main.scss'
 // 上报
-import './report'
+// import './report'
 // 图片查看器
 import Viewer from './viewer'
 // 分享
@@ -9,10 +9,12 @@ import Share from './share'
 // 边缘
 import Aside from './aside'
 
-import {addLoadEvent} from './util'
+import {
+  addLoadEvent
+} from './util'
 
-addLoadEvent(function() {
-	Share.init()
-	Viewer.init()
-	Aside.init()
+addLoadEvent(function () {
+  Share.init()
+  Viewer.init()
+  Aside.init()
 })
